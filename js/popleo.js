2cad44999184bee965eb51c7cd39ccc6649985e4
Leonel Oliveira <leoneloliver@gmail.com>
2017-05-03 13:31:46 -0400

    Update popleo.js

--- js/popleo.js
+++ js/popleo.js
@@ -1,13 +1,11 @@
 (function($){
     $("body").append("<div id='popContent'><div class='btn-popClose'>X</div><img></div>");
-    
     $("#popLeo img").each(function(index) {
       $(this).on('click',function(){
         $('#popContent img').attr('src',$(this).attr('src'))
         $('#popContent').fadeIn();    
        });
     });
-  
     $("#popContent").on('click', function(){
       $(this).fadeOut();
     });
